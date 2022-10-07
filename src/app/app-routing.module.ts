7532fcc07bef2ddccc0e0031dee0854fd255be2f
Yuwah11 <yuwah.sm@gmail.com>
2022-10-07 07:20:18 +0630

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeOneComponent } from './components/home-one/home-one.component';
 import { HomeTwoComponent } from './components/home-two/home-two.component';
-import { HomeThreeComponent } from './components/home-three/home-three.component';
-import { HomeFourComponent } from './components/home-four/home-four.component';
-import { HomeFiveComponent } from './components/home-five/home-five.component';
-import { HomeSixComponent } from './components/home-six/home-six.component';
 
 const routes: Routes = [
-    {path: '', component: HomeOneComponent},
-    {path: 'home-two', component: HomeTwoComponent},
-    {path: 'home-three', component: HomeThreeComponent},
-    {path: 'home-four', component: HomeFourComponent},
-    {path: 'home-five', component: HomeFiveComponent},
-    {path: 'home-six', component: HomeSixComponent}
+    {path: '', component: HomeTwoComponent},
+    {path: 'home-two', component: HomeTwoComponent}
 ];
 
 @NgModule({
